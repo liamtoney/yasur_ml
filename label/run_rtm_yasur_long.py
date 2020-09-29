65045f0b3e75fd3c9a7ac9f19d3acd28fb96f13c
Liam Toney <ldtoney@alaska.edu>
2020-09-29 11:15:56 -0800

    Format with black and isort

--- label/run_rtm_yasur_long.py
+++ label/run_rtm_yasur_long.py
@@ -1,31 +1,31 @@
 from obspy import UTCDateTime
+from rtm import (calculate_time_buffer, define_grid, get_peak_coordinates,
+                 grid_search, plot_time_slice, process_waveforms, produce_dem)
 from waveform_collection import gather_waveforms
-from rtm import define_grid, produce_dem, process_waveforms, calculate_time_buffer, grid_search
-from rtm import (plot_time_slice, get_peak_coordinates)
 
 # curl -O https://cloud.sdsc.edu/v1/AUTH_opentopography/hosted_data/OTDS.072019.4326.1/raster/DEM_WGS84.tif
 EXTERNAL_FILE = 'label/DEM_WGS84.tif'
 
-FREQ_MIN = .2          # [Hz] Lower bandpass corner
-FREQ_MAX = 4            # [Hz] Upper bandpass corner
+FREQ_MIN = 0.2  # [Hz] Lower bandpass corner
+FREQ_MAX = 4  # [Hz] Upper bandpass corner
 
 DECIMATION_RATE = 40  # [Hz] New sampling rate to use for decimation
-SMOOTH_WIN = None        # [s] Smoothing window duration
+SMOOTH_WIN = None  # [s] Smoothing window duration
 DH = 4
 AGC_PARAMS = dict(win_sec=120, method='walker')
 
-#data params
+# data params
 STARTTIME = UTCDateTime("2016-07-29T03:00")
-ENDTIME = STARTTIME + 10*60
+ENDTIME = STARTTIME + 10 * 60
 
-SOURCE = 'IRIS' #local, IRIS
-NETWORK = '3E' #YS, 3E
+SOURCE = 'IRIS'  # local, IRIS
+NETWORK = '3E'  # YS, 3E
 STATION = 'YIF?'
 LOCATION = '*'
 CHANNEL = '*'
 
 # grid params
-#vent mid
+# vent mid
 SEARCH_LON_0 = 169.44804400000001
 SEARCH_LAT_0 = -19.528539500000001
 
@@ -39,7 +39,7 @@ CEL = 343.48
 STACK_METHOD = 'sum'  # Choose either 'sum' or 'product'
 TIME_METHOD1 = 'celerity'  # Choose either 'celerity' or 'fdtd'
 TIME_METHOD2 = 'fdtd'  # Choose either 'celerity' or 'fdtd'
-FILENAME_ROOT = 'yasur_rtm_DH2'    #output filename root prefix
+FILENAME_ROOT = 'yasur_rtm_DH2'  # output filename root prefix
 FDTD_DIR = '/Users/ldtoney/work/yasur_ml/label/fdtd/'  # Where travel time lookup table is located
 
 XY_GRID = 350
@@ -49,9 +49,14 @@ ANNOT_INT = 50
 
 #%% Create grids
 
-search_grid = define_grid(lon_0=SEARCH_LON_0, lat_0=SEARCH_LAT_0,
-                          x_radius=SEARCH_X, y_radius=SEARCH_Y,
-                          spacing=DH_SEARCH, projected=True)
+search_grid = define_grid(
+    lon_0=SEARCH_LON_0,
+    lat_0=SEARCH_LAT_0,
+    x_radius=SEARCH_X,
+    y_radius=SEARCH_Y,
+    spacing=DH_SEARCH,
+    projected=True,
+)
 
 search_dem = produce_dem(search_grid, external_file=EXTERNAL_FILE, plot_output=True)
 
@@ -61,35 +66,58 @@ search_dem = produce_dem(search_grid, external_file=EXTERNAL_FILE, plot_output=T
 # Automatically determine appropriate time buffer in s
 time_buffer = calculate_time_buffer(search_grid, MAX_STATION_DIST)
 
-st = gather_waveforms(source=SOURCE, network=NETWORK, station=STATION,
-                   location=LOCATION, channel=CHANNEL, starttime=STARTTIME,
-                   endtime=ENDTIME, time_buffer=time_buffer)
+st = gather_waveforms(
+    source=SOURCE,
+    network=NETWORK,
+    station=STATION,
+    location=LOCATION,
+    channel=CHANNEL,
+    starttime=STARTTIME,
+    endtime=ENDTIME,
+    time_buffer=time_buffer,
+)
 
-st_proc_semb = process_waveforms(st, freqmin=FREQ_MIN, freqmax=FREQ_MAX,
-                            envelope=False, smooth_win=SMOOTH_WIN,
-                            decimation_rate=DECIMATION_RATE,
-                            agc_params=AGC_PARAMS,
-                            normalize=True, plot_steps=False)
+st_proc_semb = process_waveforms(
+    st,
+    freqmin=FREQ_MIN,
+    freqmax=FREQ_MAX,
+    envelope=False,
+    smooth_win=SMOOTH_WIN,
+    decimation_rate=DECIMATION_RATE,
+    agc_params=AGC_PARAMS,
+    normalize=True,
+    plot_steps=False,
+)
 
 #%% process grid
 
-time_kwargs_fdtd = {"FILENAME_ROOT" : FILENAME_ROOT, "FDTD_DIR": FDTD_DIR}
-S_fdtd_semb = grid_search(processed_st=st_proc_semb, grid=search_grid,
-                                  time_method=TIME_METHOD2,
-                                  starttime=STARTTIME, endtime=ENDTIME,
-                                  window=5, overlap=.5,
-                                  stack_method='semblance',
-                                  **time_kwargs_fdtd)
+time_kwargs_fdtd = {"FILENAME_ROOT": FILENAME_ROOT, "FDTD_DIR": FDTD_DIR}
+S_fdtd_semb = grid_search(
+    processed_st=st_proc_semb,
+    grid=search_grid,
+    time_method=TIME_METHOD2,
+    starttime=STARTTIME,
+    endtime=ENDTIME,
+    window=5,
+    overlap=0.5,
+    stack_method='semblance',
+    **time_kwargs_fdtd
+)
 
 #%% plot
 
-fig_slice_semb = plot_time_slice(S_fdtd_semb, st_proc_semb, time_slice=None,
-                                 label_stations=True, dem=search_dem,
-                                 plot_peak=True, xy_grid=XY_GRID, cont_int=CONT_INT,
-                                 annot_int=ANNOT_INT)
+fig_slice_semb = plot_time_slice(
+    S_fdtd_semb,
+    st_proc_semb,
+    time_slice=None,
+    label_stations=True,
+    dem=search_dem,
+    plot_peak=True,
+    xy_grid=XY_GRID,
+    cont_int=CONT_INT,
+    annot_int=ANNOT_INT,
+)
 
-time_max_semb, y_max_semb, x_max_semb, _, _ = get_peak_coordinates(S_fdtd_semb,
-                                                                   global_max=True,
-                                                                   height=.6,
-                                                                   min_time=2,
-                                                                   unproject=False)
+time_max_semb, y_max_semb, x_max_semb, _, _ = get_peak_coordinates(
+    S_fdtd_semb, global_max=True, height=0.6, min_time=2, unproject=False
+)
