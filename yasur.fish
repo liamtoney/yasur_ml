448afd383238a4d3e6eea6184da6ca784be6de11
Liam Toney <ldtoney@alaska.edu>
2020-11-15 23:55:27 -0900

    Add file containing project-related env variables, sourced by config.fish

---
+++ yasur.fish
@@ -0,0 +1,4 @@
+# Vent A and vent C colors, taken from first two entries in "New Tableau 10",
+# see https://www.tableau.com/about/blog/2016/7/colors-upgrade-tableau-10-56782
+set --export VENT_A '#4e79a7'
+set --export VENT_C '#f28e2b'
