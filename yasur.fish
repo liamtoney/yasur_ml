2850c9a00e6a5948187fef365dfb226456beb2c5
Liam Toney <ldtoney@alaska.edu>
2021-01-15 13:09:30 -0900

    Add path to manuscript figure directory

--- yasur.fish
+++ yasur.fish
@@ -2,3 +2,6 @@
 # see https://www.tableau.com/about/blog/2016/7/colors-upgrade-tableau-10-56782
 set --export VENT_A '#4e79a7'
 set --export VENT_C '#f28e2b'
+
+# Dropbox figure directory (for Overleaf manuscript)
+set --export YASUR_FIGURE_DIR ~/Dropbox/Apps/Overleaf/yasur_ml/figures
