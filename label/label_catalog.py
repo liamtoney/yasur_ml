387639888be76bce6c506f53b25ca0814e67d71a
Liam Toney <ldtoney@alaska.edu>
2021-02-02 17:24:36 -0900

    Turn plotting off for script use

--- label/label_catalog.py
+++ label/label_catalog.py
@@ -17,6 +17,9 @@ from matplotlib.ticker import PercentFormatter
 from obspy import Stream, UTCDateTime, read
 from rtm import define_grid, produce_dem
 
+# Toggle plotting
+PLOT = False
+
 # Define project directory
 WORKING_DIR = Path.home() / 'work' / 'yasur_ml'
 
@@ -71,7 +74,6 @@ A_max = hist_A.where(hist_A == hist_A.max(), drop=True)
 C_max = hist_C.where(hist_C == hist_C.max(), drop=True)
 
 # Plot
-PLOT = True
 if PLOT:
     fig, ax = plt.subplots()
     dem.plot.contour(ax=ax, levels=20, colors='black', linewidths=0.5)
@@ -122,72 +124,74 @@ df_locs = df[~pd.isnull(df.vent)]
 
 #%% (OPTIONAL) Make area plot of labeled catalog
 
-# Toggle plotting fraction of vent A vs. vent C in each window (otherwise plot totals)
-FRACTION = False
+if PLOT:
 
-# [s] Rolling window duration
-WINDOW = 60 * 60
+    # Toggle plotting fraction of vent A vs. vent C in each window (otherwise plot totals)
+    FRACTION = False
 
-# Start and end on whole hours
-t_start = UTCDateTime('2016-07-27T05')
-t_end = UTCDateTime('2016-08-01T22')
+    # [s] Rolling window duration
+    WINDOW = 60 * 60
 
-# Form array of UTCDateTimes
-t_vec = [t_start + t for t in np.arange(0, (t_end - t_start), WINDOW)]
+    # Start and end on whole hours
+    t_start = UTCDateTime('2016-07-27T05')
+    t_end = UTCDateTime('2016-08-01T22')
 
-# In moving windows, get counts of vent A and vent C
-fraction_A = []
-fraction_C = []
-for t in t_vec:
-    df_hr = df_locs[(df_locs.t >= t) & (df_locs.t < t + WINDOW)]
-    vcounts = df_hr.vent.value_counts()
+    # Form array of UTCDateTimes
+    t_vec = [t_start + t for t in np.arange(0, (t_end - t_start), WINDOW)]
+
+    # In moving windows, get counts of vent A and vent C
+    fraction_A = []
+    fraction_C = []
+    for t in t_vec:
+        df_hr = df_locs[(df_locs.t >= t) & (df_locs.t < t + WINDOW)]
+        vcounts = df_hr.vent.value_counts()
+        if FRACTION:
+            vcounts /= vcounts.sum()
+        if hasattr(vcounts, 'A'):
+            fraction_A.append(vcounts.A)
+        else:
+            fraction_A.append(0)
+        if hasattr(vcounts, 'C'):
+            fraction_C.append(vcounts.C)
+        else:
+            fraction_C.append(0)
+
+    # Load in a single station's data and process (takes a while, can comment out for repeat
+    # runs)
+    tr = read(str(WORKING_DIR / 'data' / '3E_YIF1-5_50hz.pkl')).select(station='YIF3')[0]
+    tr.remove_response()
+    tr.filter('bandpass', freqmin=0.2, freqmax=4, zerophase=True)
+
+    fig, axes = plt.subplots(nrows=2, sharex=True, figsize=(13, 5))
+
+    # Subplot 1: Waveform
+    axes[0].plot(tr.times('matplotlib'), tr.data, linewidth=0.5, color='black')
+    axes[0].set_ylabel('Pressure (Pa)')
+
+    # Subplot 2: Stacked area plot
+    t_vec_mpl = [t.matplotlib_date for t in t_vec]
+    axes[1].stackplot(
+        t_vec_mpl,
+        fraction_A,
+        fraction_C,
+        colors=(os.environ['VENT_A'], os.environ['VENT_C']),
+        labels=('Vent A', 'Vent C'),
+    )
     if FRACTION:
-        vcounts /= vcounts.sum()
-    if hasattr(vcounts, 'A'):
-        fraction_A.append(vcounts.A)
+        axes[1].yaxis.set_major_formatter(PercentFormatter(1))
     else:
-        fraction_A.append(0)
-    if hasattr(vcounts, 'C'):
-        fraction_C.append(vcounts.C)
-    else:
-        fraction_C.append(0)
+        axes[1].set_ylabel('Number of labeled events')
+    axes[1].autoscale(enable=True, axis='y', tight=True)
 
-# Load in a single station's data and process (takes a while, can comment out for repeat
-# runs)
-tr = read(str(WORKING_DIR / 'data' / '3E_YIF1-5_50hz.pkl')).select(station='YIF3')[0]
-tr.remove_response()
-tr.filter('bandpass', freqmin=0.2, freqmax=4, zerophase=True)
+    # Overall x-axis formatting
+    axes[-1].set_xlim(t_start.matplotlib_date, (t_end - WINDOW).matplotlib_date)
+    loc = axes[-1].xaxis.set_major_locator(mdates.AutoDateLocator())
+    axes[-1].xaxis.set_major_formatter(mdates.ConciseDateFormatter(loc))
 
-fig, axes = plt.subplots(nrows=2, sharex=True, figsize=(13, 5))
+    # Add legend
+    axes[-1].legend(loc='lower right')
 
-# Subplot 1: Waveform
-axes[0].plot(tr.times('matplotlib'), tr.data, linewidth=0.5, color='black')
-axes[0].set_ylabel('Pressure (Pa)')
-
-# Subplot 2: Stacked area plot
-t_vec_mpl = [t.matplotlib_date for t in t_vec]
-axes[1].stackplot(
-    t_vec_mpl,
-    fraction_A,
-    fraction_C,
-    colors=(os.environ['VENT_A'], os.environ['VENT_C']),
-    labels=('Vent A', 'Vent C'),
-)
-if FRACTION:
-    axes[1].yaxis.set_major_formatter(PercentFormatter(1))
-else:
-    axes[1].set_ylabel('Number of labeled events')
-axes[1].autoscale(enable=True, axis='y', tight=True)
-
-# Overall x-axis formatting
-axes[-1].set_xlim(t_start.matplotlib_date, (t_end - WINDOW).matplotlib_date)
-loc = axes[-1].xaxis.set_major_locator(mdates.AutoDateLocator())
-axes[-1].xaxis.set_major_formatter(mdates.ConciseDateFormatter(loc))
-
-# Add legend
-axes[-1].legend(loc='lower right')
-
-fig.show()
+    fig.show()
 
 #%% Load in full dataset
 
