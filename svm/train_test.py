e95e235430883c94f4561ba384c19a960d2b82db
Liam Toney <ldtoney@alaska.edu>
2021-04-15 17:19:12 -0800

    Commit sketchy code w/ functions

--- svm/train_test.py
+++ svm/train_test.py
@@ -13,13 +13,118 @@ from sklearn.model_selection import train_test_split
 from sklearn.utils import resample
 
 # Toggle plotting (for script use, mainly)
-PLOT = False
+PLOT = True
 
 # Define project directory
 WORKING_DIR = Path.home() / 'work' / 'yasur_ml'
 
 # Maximum iterations for SVC classifier
 MAX_ITER = 5000
+
+
+def read_and_preprocess(features_csv_file):
+    """Read in a features CSV file and perform basic pre-processing.
+
+    Args:
+        features_csv_file (str or Path): Full path to input features CSV file (must have
+            a "time" column)
+
+    Returns:
+        pandas.DataFrame: Output features
+    """
+
+    # Read in labeled features
+    features = pd.read_csv(features_csv_file)
+
+    # Convert times to UTCDateTime
+    features.time = [UTCDateTime(t) for t in features.time]
+
+    # Remove constant features
+    for column in features.columns:
+        if np.unique(features[column]).size == 1:
+            features.drop(columns=[column], inplace=True)
+
+    # Remove rows with NaNs
+    features.dropna(inplace=True)
+
+    return features
+
+
+def balance_classes(features):
+    """Function to adjust for class imbalance by down-sampling the majority class.
+
+    See https://elitedatascience.com/imbalanced-classes for more info.
+
+    Args:
+        features (pandas.DataFrame): Input features (must have a "label" column)
+
+    Returns:
+        pandas.DataFrame: Output features
+    """
+
+    # Perform the balancing
+    class_counts_before = features.label.value_counts()
+    print('Before:\n' + class_counts_before.to_string())
+    dominant_vent = class_counts_before.index[class_counts_before.argmax()]
+    majority = features[features.label == dominant_vent]
+    minority = features[features.label != dominant_vent]
+    majority_downsampled = resample(
+        majority,
+        replace=False,  # Sample w/o replacement
+        n_samples=minority.shape[0],  # Match number of waveforms in minority class
+        random_state=None,
+    )
+    features_downsampled = pd.concat([majority_downsampled, minority])
+
+    # Print what action was taken
+    class_counts_after = features_downsampled.label.value_counts()
+    print('After:\n' + class_counts_after.to_string())
+    num_removed = (class_counts_before - class_counts_after)[dominant_vent]
+    print(f'({num_removed} vent {dominant_vent} examples removed)')
+
+    return features_downsampled
+
+
+def plot_confusion(clf, X_test, y_test):
+    """Shallow wrapper around plot_confusion_matrix().
+
+    See
+    https://scikit-learn.org/stable/modules/generated/sklearn.metrics.plot_confusion_matrix.html
+    for more info. This function basically tweaks the standard plot for the Yasur
+    context.
+
+    Warning:
+        Double-check that the colored "A" and "C" labels are set in boldface. The
+        ~/.matplotlib/fontlist-v???.json font cache might need to be deleted and the
+        code re-run to accomplish this.
+
+    Args:
+        clf (estimator): Fitted classifier
+        X_test (numpy.ndarray): Input values
+        y_test (numpy.ndarray): Target values
+    """
+
+    cm = plot_confusion_matrix(
+        clf,
+        X_test,
+        y_test,
+        labels=[0, 1],  # Explicitly setting the order here
+        display_labels=['A', 'C'],  # Since 0 = vent A; 1 = vent C
+        cmap='Greys',
+        normalize='true',  # 'true' means the diagonal contains the TPR and TNR
+        values_format='.0%',  # Format as integer percent
+    )
+    fig = cm.figure_
+    ax = fig.axes[0]
+    ax.set_xlabel(ax.get_xlabel().replace('label', 'vent'))
+    ax.set_ylabel(ax.get_ylabel().replace('label', 'vent'))
+    for label in ax.get_xticklabels() + ax.get_yticklabels():
+        label.set_weight('bold')
+        label.set_color(os.environ[f'VENT_{label.get_text()}'])
+    ax.set_title(f'{y_test.size:,} test waveforms')
+    fig.axes[1].remove()  # Remove colorbar
+    fig.tight_layout()
+    fig.show()
 
 
 def train_test(features_path, train_size=0.75, plot=False, random_state=None):
@@ -33,43 +138,16 @@ def train_test(features_path, train_size=0.75, plot=False, random_state=None):
     """
 
     # Read in labeled features
-    features = pd.read_csv(features_path)
+    features = read_and_preprocess(features_path)
 
-    # Convert times to UTCDateTime
-    features.time = [UTCDateTime(t) for t in features.time]
-
-    # Remove constant features
-    for column in features.columns:
-        if np.unique(features[column]).size == 1:
-            features.drop(columns=[column], inplace=True)
-
-    # Remove rows with NaNs
-    features.dropna(inplace=True)
-
-    # Adjust for class imbalance by down-sampling the majority class (from
-    # https://elitedatascience.com/imbalanced-classes)
-    class_counts_before = features.label.value_counts()
-    print('Before:\n' + class_counts_before.to_string())
-    dominant_vent = class_counts_before.index[class_counts_before.argmax()]
-    majority = features[features.label == dominant_vent]
-    minority = features[features.label != dominant_vent]
-    majority_downsampled = resample(
-        majority,
-        replace=False,  # Sample w/o replacement
-        n_samples=minority.shape[0],  # Match number of waveforms in minority class
-        random_state=random_state,
-    )
-    features_downsampled = pd.concat([majority_downsampled, minority])
-    class_counts_after = features_downsampled.label.value_counts()
-    print('After:\n' + class_counts_after.to_string())
-    num_removed = (class_counts_before - class_counts_after)[dominant_vent]
-    print(f'({num_removed} vent {dominant_vent} examples removed)')
+    # Balance classes
+    features_downsampled = balance_classes(features)
 
     # Format dataset for use with scikit-learn
     y = (features_downsampled['label'] == 'C').to_numpy(
         dtype=int
     )  # 0 = vent A; 1 = vent C
-    X = features_downsampled.iloc[:, 2:].to_numpy()  # Skipping first two columns here
+    X = features_downsampled.iloc[:, 3:].to_numpy()  # Skipping first three columns here
 
     # Rescale data to have zero mean and unit variance
     X_scaled = preprocessing.scale(X)
@@ -92,35 +170,15 @@ def train_test(features_path, train_size=0.75, plot=False, random_state=None):
 
     # Plot if desired
     if plot:
-        cm = plot_confusion_matrix(
-            clf,
-            X_test,
-            y_test,
-            labels=[0, 1],  # Explicitly setting the order here
-            display_labels=['A', 'C'],  # Since 0 = vent A; 1 = vent C
-            cmap='Greys',
-            normalize='true',  # 'true' means the diagonal contains the TPR and TNR
-            values_format='.0%',  # Format as integer percent
-        )
-        fig = cm.figure_
-        ax = fig.axes[0]
-        ax.set_xlabel(ax.get_xlabel().replace('label', 'vent'))
-        ax.set_ylabel(ax.get_ylabel().replace('label', 'vent'))
-        for label in ax.get_xticklabels() + ax.get_yticklabels():
-            label.set_weight('bold')
-            label.set_color(os.environ[f'VENT_{label.get_text()}'])
-        ax.set_title(f'{y_test.size:,} test waveforms')
-        fig.axes[1].remove()  # Remove colorbar
-        fig.tight_layout()
-        fig.show()
+        plot_confusion(clf, X_test, y_test)
 
 
 #%% Run function
 
 train_test(
-    WORKING_DIR / 'features' / 'csv' / 'YIF2_features_tsfresh.csv',
+    WORKING_DIR / 'features' / 'csv' / 'features.csv',
     train_size=0.75,
-    plot=True,
+    plot=PLOT,
 )
 
 #%% (TODO: Update for function) Feature importance plot (permutation) - bad for correlated features
@@ -134,7 +192,7 @@ if PLOT:
     result = permutation_importance(clf, X_train, y_train, n_repeats=10)
     sorted_idx = result.importances_mean.argsort()
 
-    feature_names = features.columns[2:]
+    feature_names = features.columns[3:]
     y_ticks = np.arange(0, len(feature_names))
     fig, ax = plt.subplots()
     ax.barh(y_ticks, result.importances_mean[sorted_idx], color='grey')
@@ -161,7 +219,7 @@ if PLOT:
     std = np.std([tree.feature_importances_ for tree in forest.estimators_], axis=0)
     sorted_idx = np.argsort(importances)
 
-    feature_names = features.columns[2:]
+    feature_names = features.columns[3:]
     y_ticks = np.arange(0, len(feature_names))
     fig, ax = plt.subplots()
     ax.barh(
@@ -190,7 +248,7 @@ if PLOT:
 
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 8))
 
-    feature_names = features.columns[2:]
+    feature_names = features.columns[3:]
     corr = spearmanr(X).correlation
     corr_linkage = hierarchy.ward(corr)
     dendro = hierarchy.dendrogram(
@@ -253,3 +311,63 @@ if PLOT:
         ax.set_ylim(f2.min(), f2.max())
 
     fig.show()
+
+#%% Train and test on different times
+
+# Read in labeled features
+features = read_and_preprocess(
+    WORKING_DIR / 'features' / 'csv' / 'features_tsfresh.csv'
+)
+
+#%%
+
+# Set time interval for training
+WINDOW = 'test'  # Specify if [T1, T2] window is for 'train' or 'test' data subsetting
+T1 = features.time.max() - 2 * 24 * 60 * 60
+T2 = features.time.max()
+
+# Split by time
+if WINDOW == 'train':
+    train = features[(features.time >= T1) & (features.time <= T2)]
+    test = features[(features.time < T1) | (features.time > T2)]
+elif WINDOW == 'test':
+    train = features[(features.time < T1) | (features.time > T2)]
+    test = features[(features.time >= T1) & (features.time <= T2)]
+else:
+    raise ValueError('Window must be either \'train\' or \'test\'')
+if train.shape[0] + test.shape[0] != features.shape[0]:
+    raise ValueError('Temporal subsetting failed due to dimension mismatch!')
+
+# Balance classes
+print('TRAINING')
+train_ds = balance_classes(train)
+print('\nTESTING')
+test_ds = balance_classes(test)
+
+# Format dataset for use with scikit-learn
+y_train = (train_ds['label'] == 'C').to_numpy(dtype=int)  # 0 = vent A; 1 = vent C
+X_train = train_ds.iloc[:, 3:].to_numpy()  # Skipping first three columns here
+y_test = (test_ds['label'] == 'C').to_numpy(dtype=int)  # 0 = vent A; 1 = vent C
+X_test = test_ds.iloc[:, 3:].to_numpy()  # Skipping first three columns here
+
+# Rescale data to have zero mean and unit variance
+X_train = preprocessing.scale(X_train)
+X_test = preprocessing.scale(X_test)
+
+print(
+    f'\nTraining portion: {train_ds.shape[0] / (train_ds.shape[0] + test_ds.shape[0]) * 100:g}%'
+)
+print(f'Training size: {y_train.size}')
+print(f'Testing size: {y_test.size}')
+
+# Run SVC
+clf = svm.LinearSVC(max_iter=MAX_ITER)
+clf.fit(X_train, y_train)
+
+# Test SVC
+score = clf.score(X_test, y_test)
+print(f'\nAccuracy is {score:.0%}')
+
+# Plot if desired
+if PLOT:
+    plot_confusion(clf, X_test, y_test)
