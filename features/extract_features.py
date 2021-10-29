556b55705ba40abe777e1b6242e8a8a092071dd1
Liam Toney <ldtoney@alaska.edu>
2021-10-28 16:52:35 -0800

    Overhaul feature extraction script

--- features/extract_features.py
+++ features/extract_features.py
@@ -2,7 +2,6 @@
 
 from pathlib import Path
 
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from obspy import read
@@ -17,80 +16,53 @@ WORKING_DIR = Path.home() / 'work' / 'yasur_ml'
 labeled_wf_dir = WORKING_DIR / 'data' / 'labeled'
 
 # Toggle bandpass filtering of data
-FILTER = True
+FILTER = False
 
 # Bandpass filter corners [Hz]
 FREQMIN = 0.2
 FREQMAX = 4
 
-# Toggle using TSFRESH
-TSFRESH = True
+# Toggle randomly shifting travel times via np.roll()
+ROLL = False
 
-# Toggle randomly shifting travel times via np.roll() (TSFRESH only)
-ROLL = True
+# Specify 'manual' or 'tsfresh' feature set
+SET_TYPE = 'tsfresh'
 
-#%% Option 1: Manual feature engineering
 
-if not TSFRESH:
+def manual_extractor(st):
+    """
+    Extracts 10 "hand-picked" manually defined features.
+    """
 
     FFT_WIN_DUR = 5  # [s]
 
     # Initialize DataFrame of extracted features
-    features = pd.DataFrame(
-        columns=[
-            'station',
-            'time',  # Origin time from catalog
-            'label',
-            'td_std',
-            'td_skewness',
-            'td_kurtosis',
-            'fd_std',
-            'fd_skewness',
-            'fd_kurtosis',
-            'fd_q1',
-            'fd_q2',
-            'fd_q3',
-            'fd_peak',
-        ]
-    )
+    features_st = pd.DataFrame()
 
-    # Iterate over all labeled waveform files
-    for file in sorted(labeled_wf_dir.glob('label_???.pkl')):
+    # Calculate features, append to DataFrame
+    for tr in st:
 
-        # Read in
-        print(f'Reading {file}')
-        st = read(str(file))  # Use all stations
+        # Transform to frequency domain
+        fs = tr.stats.sampling_rate
+        nperseg = int(FFT_WIN_DUR * fs)  # Samples
+        nfft = np.power(2, int(np.ceil(np.log2(nperseg))) + 3)  # Pad FFT
+        f, psd = welch(tr.data, fs, nperseg=nperseg, nfft=nfft)
 
-        # Process
-        st.remove_response()
-        st.taper(0.01)
-        if FILTER:
-            st.filter('bandpass', freqmin=FREQMIN, freqmax=FREQMAX, zerophase=True)
-        st.normalize()
+        # Get CSD, normalize
+        csd = np.cumsum(psd)
+        csd_norm = csd / csd.max()
+        psd_norm = psd / psd.max()
 
-        # Calculate features, append to DataFrame
-        for tr in st:
+        # Find quartiles
+        quartiles = {}
+        for quartile in 0.25, 0.5, 0.75:
+            idx = (np.abs(csd_norm - quartile)).argmin()
+            quartiles[quartile] = f[idx]
 
-            # Transform to frequency domain
-            fs = tr.stats.sampling_rate
-            nperseg = int(FFT_WIN_DUR * fs)  # Samples
-            nfft = np.power(2, int(np.ceil(np.log2(nperseg))) + 3)  # Pad FFT
-            f, psd = welch(tr.data, fs, nperseg=nperseg, nfft=nfft)
-
-            # Get CSD, normalize
-            csd = np.cumsum(psd)
-            csd_norm = csd / csd.max()
-            psd_norm = psd / psd.max()
-
-            # Find quartiles
-            quartiles = {}
-            for quartile in 0.25, 0.5, 0.75:
-                idx = (np.abs(csd_norm - quartile)).argmin()
-                quartiles[quartile] = f[idx]
-
-            info = dict(
+        features_tr = pd.DataFrame(
+            dict(
                 station=tr.stats.station,
-                time=tr.stats.event_info.origin_time,
+                time=tr.stats.event_info.origin_time,  # Origin time from catalog
                 label=tr.stats.vent,
                 td_std=np.std(tr.data),
                 td_skewness=stats.skew(tr.data),
@@ -102,103 +74,89 @@ if not TSFRESH:
                 fd_q2=quartiles[0.5],  # [Hz]
                 fd_q3=quartiles[0.75],  # [Hz]
                 fd_peak=f[np.argmax(psd)],  # [Hz]
-            )
-            features = features.append(info, ignore_index=True)
-
-    # Save as CSV
-    filename = 'features.csv'
-    if FILTER:
-        filename = filename.replace('.csv', '_filtered.csv')
-    features.to_csv(WORKING_DIR / 'features' / 'csv' / filename, index=False)
-
-    #%% (OPTIONAL) Example plot of frequency domain features
-
-    fig, axes = plt.subplots(
-        ncols=2, figsize=(6, 3), sharey=True, gridspec_kw=dict(width_ratios=[0.7, 0.3])
-    )
-
-    axes[0].plot(f, psd_norm, label='PSD')
-    axes[0].plot(f, csd_norm, '--', color='black', label='CSD')
-    axes[0].scatter(
-        [v for v in quartiles.values()],
-        [k for k in quartiles.keys()],
-        color='black',
-        label='Quartiles',
-        zorder=100,
-    )
-    axes[0].set_xlabel('Frequency (Hz)')
-    axes[0].set_ylabel('Normalized PSD/CSD')
-    axes[0].autoscale(tight=True)
-    axes[0].legend()
-
-    axes[1].hist(psd_norm, bins=20, orientation='horizontal')
-    axes[1].set_xlabel('PSD counts')
-    axes[1].tick_params(labelleft=False)
-    axes[1].autoscale(tight=True)
-
-    fig.tight_layout()
-    fig.show()
-
-#%% Option 2: TSFRESH
-
-else:
-
-    # Initialize DataFrame of extracted features
-    features = pd.DataFrame()
-
-    # Iterate over all labeled waveform files
-    for file in sorted(labeled_wf_dir.glob('label_???.pkl')):
-
-        # Read in
-        print(f'Reading {file}')
-        st = read(str(file))  # Use all stations
-
-        # Process
-        st.remove_response()
-        st.taper(0.01)
-        if FILTER:
-            st.filter('bandpass', freqmin=FREQMIN, freqmax=FREQMAX, zerophase=True)
-        st.normalize()
-
-        # Put into TSFRESH format
-        timeseries = pd.DataFrame()
-        labels = []
-        otimes = []  # Origin time from catalog
-        stations = []
-        for i, tr in enumerate(st):
-            id = pd.Series(np.ones(tr.stats.npts, dtype=int) * i)
-            time = pd.Series(tr.times())
-            if ROLL:
-                # Randomly shuffle waveforms to remove travel time biases
-                value = pd.Series(np.roll(tr.data, np.random.randint(tr.data.size)))
-            else:
-                value = pd.Series(tr.data)
-            timeseries = pd.concat(
-                [timeseries, pd.DataFrame(dict(id=id, time=time, x=value))],
-                ignore_index=True,
-            )
-            labels.append(tr.stats.vent)
-            otimes.append(tr.stats.event_info.origin_time)
-            stations.append(tr.stats.station)
-
-        # Calculate features
-        extracted_features = extract_features(
-            timeseries, column_id='id', column_sort='time'
+            ),
+            index=[0],
         )
+        features_st = pd.concat([features_st, features_tr], ignore_index=True)
 
-        # Tweak and append label, time, and station info to main DataFrame
-        extracted_features.insert(0, column='label', value=labels)
-        extracted_features.insert(0, column='time', value=otimes)
-        extracted_features.insert(0, column='station', value=stations)
-        extracted_features.columns = [
-            column.split('__', 1)[-1] for column in extracted_features.columns
-        ]
-        features = pd.concat([features, extracted_features], ignore_index=True)
+    return features_st
 
-    # Save as CSV
-    filename = 'features_tsfresh.csv'
-    if ROLL:
-        filename = filename.replace('.csv', '_roll.csv')
+
+def tsfresh_extractor(st):
+    """
+    Extracts 700+ features automatically, leveraging TSFRESH.
+    """
+
+    # Put into TSFRESH format
+    timeseries = pd.DataFrame()
+    labels = []
+    otimes = []  # Origin time from catalog
+    stations = []
+    for i, tr in enumerate(st):
+        id = pd.Series(np.ones(tr.stats.npts, dtype=int) * i)
+        time = pd.Series(tr.times())
+        value = pd.Series(tr.data)
+        timeseries = pd.concat(
+            [timeseries, pd.DataFrame(dict(id=id, time=time, x=value))],
+            ignore_index=True,
+        )
+        labels.append(tr.stats.vent)
+        otimes.append(tr.stats.event_info.origin_time)
+        stations.append(tr.stats.station)
+
+    # Calculate features
+    features_st = extract_features(timeseries, column_id='id', column_sort='time')
+
+    # Append label, time, and station info to main DataFrame (last appended = first col)
+    features_st.insert(0, column='label', value=labels)
+    features_st.insert(0, column='time', value=otimes)
+    features_st.insert(0, column='station', value=stations)
+
+    # Slightly shorten the very long TSFRESH feature [= column] names
+    features_st.columns = [column.split('__', 1)[-1] for column in features_st.columns]
+
+    return features_st
+
+
+# Choose specified feature extractor
+if SET_TYPE == 'manual':
+    feature_extractor = manual_extractor
+elif SET_TYPE == 'tsfresh':
+    feature_extractor = tsfresh_extractor
+else:
+    raise ValueError('Invalid SET_TYPE!')
+
+# Initialize main DataFrame of extracted features
+features = pd.DataFrame()
+
+# Iterate over all labeled waveform files
+for file in sorted(labeled_wf_dir.glob('label_???.pkl')):
+
+    # Read in
+    print(f'Reading {file}')
+    st = read(str(file))
+
+    # Pre-process prior to extraction
+    st.remove_response()
+    st.taper(0.01)
     if FILTER:
-        filename = filename.replace('.csv', '_filtered.csv')
-    features.to_csv(WORKING_DIR / 'features' / 'csv' / filename, index=False)
+        st.filter('bandpass', freqmin=FREQMIN, freqmax=FREQMAX, zerophase=True)
+    if ROLL:
+        # Randomly shuffle waveforms to remove travel time biases
+        for tr in st:
+            tr.data = np.roll(tr.data, np.random.randint(tr.data.size))
+    st.normalize()
+
+    # Extract features
+    features_st = feature_extractor(st)
+
+    # Append to main DataFrame
+    features = pd.concat([features, features_st], ignore_index=True)
+
+# Save as CSV
+filename = f'{SET_TYPE}.csv'
+if FILTER:
+    filename = filename.replace('.csv', '_filter.csv')
+if ROLL:
+    filename = filename.replace('.csv', '_roll.csv')
+features.to_csv(WORKING_DIR / 'features' / 'csv' / filename, index=False)
