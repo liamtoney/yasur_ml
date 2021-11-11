828829a9d3741bfe9bf8ff83d381f41d512fd5b1
Liam Toney <ldtoney@alaska.edu>
2021-11-10 18:36:31 -0900

    Flesh out first draft of gen matrix figure

--- plot_scripts/generalization/generalization.py
+++ plot_scripts/generalization/generalization.py
@@ -1,3 +1,5 @@
+import os
+import subprocess
 from pathlib import Path
 
 import matplotlib.pyplot as plt
@@ -25,8 +27,41 @@ tsfresh_scores = np.load(
 
 #%% Plot
 
-for scores in manual_scores, tsfresh_scores:
-    fig, ax = plt.subplots()
-    plot_generalization_matrix(scores, fig, ax)
-    fig.tight_layout()
-    fig.show()
+fig = plt.figure(figsize=(14, 10))
+gs = fig.add_gridspec(ncols=3, width_ratios=[3, 3, 0.1])  # Ratios set colorbar width
+ax1 = fig.add_subplot(gs[0])
+ax2 = fig.add_subplot(gs[1], sharex=ax1, sharey=ax1)
+cax = fig.add_subplot(gs[2])
+
+# Call function
+plot_generalization_matrix(manual_scores, fig, ax1, show_stats=False, colorbar=False)
+plot_generalization_matrix(tsfresh_scores, fig, ax2, show_stats=False, colorbar=cax)
+
+# Tweak
+ax2.set_ylabel('')
+ax2.tick_params(which='both', axis='y', labelleft=False)
+fig.subplots_adjust(wspace=0.15)
+
+# Adjust colorbar position and height
+pos1 = ax1.get_position()
+posc = cax.get_position()
+cax.set_position([posc.x0, pos1.y0, posc.width, pos1.height])
+
+# Plot (a) and (b) tags
+for ax, label in zip([ax1, ax2], ['A', 'B']):
+    ax.text(
+        -0.04,
+        1.03,
+        label,
+        ha='right',
+        va='bottom',
+        transform=ax.transAxes,
+        weight='bold',
+        fontsize=18,
+    )
+
+fig.show()
+
+_ = subprocess.run(['open', os.environ['YASUR_FIGURE_DIR']])
+
+# fig.savefig(Path(os.environ['YASUR_FIGURE_DIR']) / 'generalization.png', bbox_inches='tight', dpi=300)
