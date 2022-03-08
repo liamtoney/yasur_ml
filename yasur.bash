2573996272c1a796b5baf7de19bdbc35e7b13d8b
Liam Toney <ldtoney@alaska.edu>
2022-03-08 12:10:41 -0900

    Avoid fish to make code more transportable

--- yasur.bash
+++ yasur.bash
@@ -1,5 +1,7 @@
-# Change into Yasur directory
-cd ~/work/yasur_ml/
+# Subcrater S and subcrater N colors, taken from first two entries in "New Tableau 10",
+# see https://www.tableau.com/about/blog/2016/7/colors-upgrade-tableau-10-56782
+export SUBCRATER_S='#4e79a7'
+export SUBCRATER_N='#f28e2b'
 
-# Activate yasur_ml
-conda activate yasur_ml
+# Dropbox figure directory (for Overleaf manuscript)
+export YASUR_FIGURE_DIR='~/Dropbox/Apps/Overleaf/yasur_ml/figures'
--- yasur.fish
+++
@@ -1,7 +0,0 @@
-# Subcrater S and subcrater N colors, taken from first two entries in "New Tableau 10",
-# see https://www.tableau.com/about/blog/2016/7/colors-upgrade-tableau-10-56782
-set --export SUBCRATER_S '#4e79a7'
-set --export SUBCRATER_N '#f28e2b'
-
-# Dropbox figure directory (for Overleaf manuscript)
-set --export YASUR_FIGURE_DIR ~/Dropbox/Apps/Overleaf/yasur_ml/figures
