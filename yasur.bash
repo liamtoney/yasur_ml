a951c0f25435d132156f7fd8f717edcbb0ef0a92
Liam Toney <ldtoney@alaska.edu>
2022-03-08 17:15:54 -0900

    Move some vars to YAML file

--- yasur.bash
+++
@@ -1,7 +0,0 @@
-# Subcrater S and subcrater N colors, taken from first two entries in "New Tableau 10",
-# see https://www.tableau.com/about/blog/2016/7/colors-upgrade-tableau-10-56782
-export SUBCRATER_S='#4e79a7'
-export SUBCRATER_N='#f28e2b'
-
-# Dropbox figure directory (for Overleaf manuscript)
-export YASUR_FIGURE_DIR='~/Dropbox/Apps/Overleaf/yasur_ml/figures'
